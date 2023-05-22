9b5fcf808b6226f132eeeed775b4826af121aea9
ibnaleem <134088573+ibnaleem@users.noreply.github.com>
2023-05-22 14:47:29 +0100

    Added check() and _common_check() functions

--- password-strength/main.py
+++ password-strength/main.py
@@ -1,1 +1,9 @@
-
+class Password:
+    def __init__(self):
+        self._is_common = False
+        
+        def check(self, password: str):
+          if self._common_check(password):
+            return "Your password is very common and was found in a database. You should generate a more secure one"
+          
+        def _common_check(self, password: str) -> bool:

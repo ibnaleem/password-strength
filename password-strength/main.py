e16f45e58799b9e2efcba2cdfcc1735c8c1bf886
ibnaleem <134088573+ibnaleem@users.noreply.github.com>
2023-05-22 14:54:46 +0100

    added counter + fixed code formatting

--- password-strength/main.py
+++ password-strength/main.py
@@ -4,28 +4,31 @@ import hashlib
 class Password:
     def __init__(self):
         self._is_common = False
-        
-        def check(self, password: str):
-          if self._common_check(password):
+        self._count = 0
+ 
+    def check(self, password: str):
+        if self._common_check(password):
             return "Your password is very common and was found in a database. You should generate a more secure one"
+        unique_count = self._check_special_character_diversity(password)
+        self._count += unique_count / 2        
           
-        def _common_check(self, password: str) -> bool:
-            hash_pswd = self._hash(password)
-            url = f"https://api.pwnedpasswords.com/range/{hash_pswd}"
-            response = requests.get(url)
+    def _common_check(self, password: str) -> bool:
+        hash_pswd = self._hash(password)
+        url = f"https://api.pwnedpasswords.com/range/{hash_pswd}"
+        response = requests.get(url)
 
-            if response.status_code == 200:
-                self._is_common = True
+        if response.status_code == 200:
+            self._is_common = True
 
-            return self._is_common
+        return self._is_common
 
-        def _hash(self, password: str) -> str:
-            h = hashlib.new('sha1')
-            h.update(str(password).encode()) 
-            return h.hexdigest()
+    def _hash(self, password: str) -> str:
+        h = hashlib.new('sha1')
+        h.update(str(password).encode()) 
+        return h.hexdigest()
         
-        def _check_special_character_diversity(self, password) -> int:
-            special_characters = "!@#$%^&*()_+{}[]|;:,.<>/?"
-            symbols = [char for char in password if char in special_characters]
-            unique_symbols_count = len(set(symbols))
-            return unique_symbols_count
+    def _check_special_character_diversity(self, password) -> int:
+        special_characters = "!@#$%^&*()_+{}[]|;:,.<>/?"
+        symbols = [char for char in password if char in special_characters]
+        unique_symbols_count = len(set(symbols))
+        return unique_symbols_count

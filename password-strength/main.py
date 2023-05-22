f7a985e060035f41c8fb7d1a5ad3942b481edc62
ibnaleem <134088573+ibnaleem@users.noreply.github.com>
2023-05-22 14:49:23 +0100

    added password hashing and database checker

--- password-strength/main.py
+++ password-strength/main.py
@@ -1,3 +1,6 @@
+import requests
+import hashlib
+
 class Password:
     def __init__(self):
         self._is_common = False
@@ -7,3 +10,16 @@ class Password:
             return "Your password is very common and was found in a database. You should generate a more secure one"
           
         def _common_check(self, password: str) -> bool:
+            hash_pswd = self._hash(password)
+            url = f"https://api.pwnedpasswords.com/range/{hash_pswd}"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                self._is_common = True
+
+            return self._is_common
+
+        def _hash(self, password: str) -> str:
+            h = hashlib.new('sha1')
+            h.update(str(password).encode()) 
+            return h.hexdigest()
